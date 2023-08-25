8263aeaffc1ac3b5e9ee039c56e279202e8b7f2e
kaustubh3972 <112243770+kaustubh3972@users.noreply.github.com>
2023-08-25 19:23:34 +0530

    Update client.js
    Personal url for io socket

--- js/client.js
+++ js/client.js
@@ -1,5 +1,6 @@
 // const socket = io("http://localhost:7000", { tranports: ["websocket"] });
-const socket = io("http://localhost:8000");
+// const socket = io("http://localhost:8000");
+const socket = io("https://mychatappbyk.netlify.app/");
 // const io = require("socket.io-client");
 // socket = io();
 console.log("hi client");
